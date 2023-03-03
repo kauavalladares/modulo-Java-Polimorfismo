1d171a4d4950127f5bc96a306b394e68900137fe
Kauavalladares <kaua.valldares@gmail.com>
2023-03-03 16:56:39 -0300

    modificação no projeto bytebank-herdado adição das classes FuncionarioAutentica, Sistema,

---
+++ bytenbank-herdado/src/Administrador.java
@@ -0,0 +1,10 @@
+public class Administrador extends FuncionarioAutentica {
+
+
+    @Override
+    public double getbonificacao() {
+        return 50;
+    }
+
+
+}
--- bytenbank-herdado/src/gerente.java
+++ bytenbank-herdado/src/gerente.java
@@ -1,20 +1,7 @@
 
-//gerente é um funcionario,gerente herda da class funcionario.
-public class gerente extends funcionario{
+//gerente é um funcionario autenticavel, gerente herda da class funcionario.
+public class gerente extends FuncionarioAutentica {
 
-    private int senha;
-
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
-
-    public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     public double getbonificacao() {
         System.out.println("chamando o metodo de bonificação do gerente. ");
